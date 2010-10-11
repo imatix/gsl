b59ab4cafe7dbcd25486858fbe5b50f9a5ce5dd0
Pieter Hintjens <ph@imatix.com>
2010-10-11 20:33:33 +0200

    Added gcccat needed by 'c'

---
+++ src/gcccat.bat
@@ -0,0 +1,19 @@
+@echo off
+:-
+:-  Copyright (c) 1996-2009 iMatix Corporation
+:-
+:-  This program is free software; you can redistribute it and/or modify
+:-  it under the terms of the GNU General Public License as published by
+:-  the Free Software Foundation; either version 2 of the License, or (at
+:-  your option) any later version.
+:-
+:-  This program is distributed in the hope that it will be useful, but
+:-  WITHOUT ANY WARRANTY; without even the implied warranty of
+:-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+:-  General Public License for more details.
+:-
+:-  For information on alternative licensing for OEMs, please contact
+:-  iMatix Corporation.
+:-
+
+echo Thank you for trying gcccat.  Please install gcc to continue...
