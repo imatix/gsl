9a92b54eb391575cc92a174241ad2d8d21fc0394
evoskuil <eric@voskuil.org>
2014-04-25 04:13:10 -0700

    Update packaging, package id and tool name change to gsl.exe.

--- packaging/nuget/package.bat
+++ packaging/nuget/package.bat
@@ -7,7 +7,5 @@ ECHO.
 REM: http://nuget.codeplex.com/releases
 nuget pack package.nuspec -verbosity detailed
 ECHO.
-ECHO NOTE: Ignore warnings not applicable to native code: "Issue: Assembly outside lib folder."
-ECHO.
 ECHO Completed nuget packaging build. The package is in the following folder:
 CD
