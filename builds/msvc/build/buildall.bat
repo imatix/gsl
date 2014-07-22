484893dfbfa7facbb5d97315da71ed706c52a13a
evoskuil <eric@voskuil.org>
2014-07-22 01:12:08 -0500

    pull nuget packages on batch build.

--- builds/msvc/build/buildall.bat
+++ builds/msvc/build/buildall.bat
@@ -1,8 +1,10 @@
 @ECHO OFF
-
+ECHO.
+ECHO Downloading gsl dependencies from NuGet
+CALL nuget.exe install ..\vs2012\gsl\packages.config
+ECHO.
 CALL buildbase.bat ..\vs2012\gsl.sln 11
 ECHO.
 CALL buildbase.bat ..\vs2010\gsl.sln 10
 ECHO.
-
 PAUSE
