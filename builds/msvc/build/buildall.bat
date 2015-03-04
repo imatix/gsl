3ae2b5927ca1e2014596bcd2084b3f528e1bb3a4
evoskuil <eric@voskuil.org>
2015-03-03 18:52:31 -0800

    Decentralize the NuGet repo (VS2012/2010).

--- builds/msvc/build/buildall.bat
+++ builds/msvc/build/buildall.bat
@@ -1,7 +1,9 @@
 @ECHO OFF
 ECHO.
 ECHO Downloading gsl dependencies from NuGet
-CALL nuget.exe install ..\vs2012\gsl\packages.config
+REM Yes this is redundant, but much simpler then a central repo.
+CALL nuget.exe install ..\vs2012\gsl\packages.config -o ..\vs2012\packages
+CALL nuget.exe install ..\vs2010\gsl\packages.config -o ..\vs2010\packages
 ECHO.
 CALL buildbase.bat ..\vs2012\gsl.sln 11
 ECHO.
